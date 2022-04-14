fcaf2da5c630860d724235d67c4a3f0c6c60c510
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-14 18:38:29 -0500

    users finds users correctly, but nothing else works

---
+++ api/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const server = express();
+
+
+
+// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
+// global middlewares and the user's router need to be connected here
+
+const Users = require('./users/users-model')
+// const Register = require('./register/register-model')
+// const Login = require('./login/login-model')
+
+// User Endpoint
+/*| GET    | /api/users    | Returns an array users.   */
+server.get('/api/users', (req, res) => {
+    Users.find(req.query)
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved" 
+        })
+    })
+});
+
+// Register Endpoint
+server.post('./api/register', (req, res) => {
+    let user = req.body
+    Users.addNewUser(user)
+        .then(newUser => {
+            if(!newUser) {
+                res.status(400).json({message: "Please provide name and password for the user"})
+            } else {
+                res.status(201).json(newUser);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the user to the database" 
+            })
+        })
+})
+
+// Login Endpoint
+// server.post('./api/login', (req, res) => {
+
+// })
+
+// General Endpoint
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+// Export
+module.exports = server;
---
+++ api/users/users-model.js
@@ -0,0 +1,25 @@
+  
+let users = [
+        { name: 'Ed', password: "12345" },
+        { name: 'Mary', password: "54321" },
+]
+
+
+const find = () => {
+    return Promise.resolve(users)
+}
+
+
+const addNewUser = ({ name, password }) => {
+    // INSERT INTO users (name, bio) VALUES ('foo', 'bar');
+    const newUser = { name, password }
+    users.push(newUser)
+    console.log(users)
+    return Promise.resolve(newUser)
+}
+
+// const checkLoginInfo = ({ name, password }) => {
+//     const user = users.find(user => user.username === username && user.password === password)
+// }
+
+module.exports = { find, addNewUser };
